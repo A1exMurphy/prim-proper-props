f42c479851ba71f7d91e7a1604cb65212eb28033
Evan Langer <elanger@live.com>
2023-11-21 16:54:28 -0600

    made GuestList comp and src'd in App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,6 +3,7 @@ import axios from 'axios';
 import './App.css';
 import Header from '../Header/Header.jsx';
 import Footer from '../Footer/Footer.jsx';
+import CompileGuestList from '../GuestList/GuestList.jsx'
 
 function App() {
   let [guestList, setGuestList] = useState([]);
@@ -101,22 +102,7 @@ function App() {
         <button type="submit">Add Guest</button>
       </form>
       <h2>Guest List</h2>
-      <table>
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Kid's Meal</th>
-          </tr>
-        </thead>
-        <tbody>
-          {guestList.map(guest => (
-            <tr key={guest.id}>
-              <td>{guest.name}</td>
-              <td>{String(guest.kidsMeal)}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+          <CompileGuestList guestList={guestList}/>
       <h2>Dinner Supplies</h2>
       <div>
         Spoons: {guestList.length * 2}
