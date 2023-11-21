71c06e42f80d52050b3d131769a12575254bbb71
Evan Langer <elanger@live.com>
2023-11-21 16:29:35 -0600

    made Header comp and src'd in App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,8 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 import './App.css';
+import Header from '../Header/Header.jsx';
+import Footer from '../Footer/Footer.jsx';
 
 function App() {
   let [guestList, setGuestList] = useState([]);
@@ -53,9 +55,7 @@ function App() {
   console.log(newGuestMeal)
   return (
     <div className="App">
-      <header>
-        <h1>Prim Proper Props</h1>
-      </header>
+      <Header />
       <h2>Party Leader</h2>
       {guestList[0] && <h3>{guestList[0].name}</h3>}
       <h2>Add a new guest</h2>
@@ -128,8 +128,7 @@ function App() {
         Knives: {guestList.length * 2}
       </div>
       <footer>
-        <h3>Have fun!</h3>
-        <p>Don't forget to mind your Ps and Qs!</p>
+        <Footer />
       </footer>
     </div>
   );
